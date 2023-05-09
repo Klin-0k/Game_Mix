8378a0dfad371356f91a6948709b2673a8e6eec9
aucalka <114307016+aucalka@users.noreply.github.com>
2023-05-10 01:58:45 +0300

    Checkpoint 3 savchenko (#9)
    * fix
    
    * fix_final(maybe)_1
    
    * checkpoint_2
    
    * small_fix
    
    * small_fix
    
    * small_fix
    
    * game2 added
    
    * game2 added
    
    * game2 added + small fix
    
    * qq
    
    * commit to do pull after
    
    * another commit to do pull after
    
    * Finished(?) Game1
    
    ---------
    
    Co-authored-by: Ivan <Klinitski.ia@phystech.edu>
    Co-authored-by: Alexandr Dmitrievich <dsasha310105@gmail.com>
    Co-authored-by: aucalka <savchenko.yd@yandex.edu>

--- Global_Classes.h
+++ Global_Classes.h
@@ -3,7 +3,6 @@
 #include <iostream>
 #include <unordered_set>
 #include <fstream>
-#include <iostream>
 
 #include <SFML/Graphics.hpp>
 
@@ -390,13 +389,19 @@ struct Figure {
 class Game1 : Essence {
  private:
   Game1();
-  void CheckEvents(const sf::Event& event);
-  void DestroyRow(int i, int j);
-  sf::Color GetRandomColor();
-  Object* game_background;
+
   static Game1 *game_1_pointer;
+
   Fon *background;
+
+  Object* game_background;
+  Object* game_over_background;
   sf::Texture game_background_image;
+  sf::Texture game_over_background_image;
+
+  Button* back_button;
+  Button* restart_button;
+
   static const int width_of_net_in_tiles = 15;
   int height_of_net_in_tiles;
   float indent_x;
@@ -405,15 +410,17 @@ class Game1 : Essence {
   sf::RectangleShape tile;
 
   sf::RectangleShape red_line;
-  sf::RectangleShape white_line;
+  sf::RectangleShape white_line1;
+  sf::RectangleShape white_line2;
+  sf::RectangleShape white_line3;
+  sf::RectangleShape white_line4;
 
   std::vector<std::vector<Coords>> figures_pos;
   std::vector<std::vector<sf::RectangleShape>> figures;
   int type_of_figure;
   Figure figure;
   Figure next_figure;
-  sf::Color color;
-  sf::Color next_color;
+
   int speed_of_falling;
   int moves_by_x;
   int moves_by_y;
@@ -427,24 +434,82 @@ class Game1 : Essence {
 
   int start_transparency = 255;
   int transparency = start_transparency;
+
   int speed_of_destruction = 5;
   bool in_process_of_destruction = false;
   int row_of_destruction = -1;
   int counter_of_destructed_rows = 0;
   int max_value_before_speed_up = 5;
 
+  bool is_active_game_over = false;
   int score = 0;
-  //record = get_record();
-  int rows = 0;
+
   int one_tile_cost = 10;
+  int coins;
+  int record;
+
+  sf::Font main_font;
+  sf::Text record_text;
+  sf::Text record_value;
+  sf::Text score_text;
+  sf::Text score_value;
+  sf::Text coins_text;
+  sf::Text coins_value;
+  sf::Text new_text;
+  sf::Text back_text;
+  sf::Text restart_text;
+  sf::Text game_over_text;
+
+  sf::Color color;
+  sf::Color next_color;
+  sf::Color my_green = {41, 232, 35};
+  sf::Color my_blue = {20, 54, 247};
+  sf::Color my_orange = {252, 233, 56};
+  sf::Color my_skyblue = {187, 237, 234};
+  sf::Color my_white = {255, 255, 255};
+  sf::Color my_transparent = {255, 255, 255, 0};
 
  public:
   static Game1* GetGame1();
   Game1(const Game1&) = delete;
   Game1& operator=(const Game1&) = delete;
-  void stop();
-  void cont();
+
   void Update(double dt);
   void Draw();
+
+  void CreateBackground();
+  void CreateButtons();
+  void SetTileProperties();
+  void CreateDesignLines();
+  void CreateFigures();
+  void SetFigureMovement();
+  void CreateDataArrays();
+  void CreateTextObjects();
+  void DrawNet();
+  void DrawFigure();
+  void DrawNextFigure();
+  void MaintainCorrectBottom();
+  void SetNetTile();
+  void MoveByX(const Figure& prev_figure);
+  void MoveByY(const Figure& prev_figure);
+  void Rotate(const Figure& prev_figure);
+  void UpdateCounters();
+  void ReturnToBase();
+  void FindDestroyedRows();
+  void GetGameOver();
+
+  sf::Color GetRandomColor();
+  int GetRecord();
+  void SetRecord(int my_record, int my_score);
+  void SetText(sf::Text& target, const sf::Font& font, const std::string& text, int character_size, sf::Color my_color,
+               sf::Vector2f pos);
+
+  void CheckEvents(const sf::Event& event);
+  void DestroyRow(int i, int j);
+  void ReloadNet();
+
+  void ButtonLink(const sf::Event& event);
+  void ReloadGame(const sf::Event& event);
+
   ~Game1();
 };
