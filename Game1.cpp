c7a7f9358c9a6fcf3550558d58dab15d1d8176b8
aucalka <savchenko.yd@yandex.edu>
2023-05-10 01:36:43 +0300

    Finished(?) Game1

--- Game1.cpp
+++ Game1.cpp
@@ -3,37 +3,87 @@
 #include <stdlib.h>
 #include <time.h>
 
-Game1::Game1() {
-  std::srand(time(NULL));
+void Game1::ButtonLink(const sf::Event &event) {
+  PlayMenu::getPlayMenu()->coins+=coins;
+  SetRecord(record, score);
+  ReloadNet();
+  game_1_pointer->Delete();
+}
 
+void Game1::ReloadGame(const sf::Event& event) {
+  is_active_game_over = false;
+  ReloadNet();
+  score = 0;
+}
+
+void Game1::CreateBackground() {
   background = new Fon(PATH_TO_RESOURCES"/Game1/images/background*.png",
                        1, 0, parent_, false);
   game_background_image.loadFromFile(PATH_TO_RESOURCES"/Game1/images/game_background.jpg");
   game_over_background_image.loadFromFile(PATH_TO_RESOURCES"/Game1/images/game_over_background.jpg");
-  SetUpdateEvent([this] (double dt) {Update(dt); });
   game_background =  new Object(game_background_image, parent_, false);
   game_over_background =  new Object(game_over_background_image, parent_, false);
-  indent_x = static_cast<float>(0.02 * parent_->getSize().x);
-  indent_y = static_cast<float>(0.02 * parent_->getSize().y);
   game_background->Move(indent_x, indent_y);
   game_background->SetScale(static_cast<float>(0.5 * parent_->getSize().x) / game_background->GetLocalWidth(),
                             static_cast<float>(0.96 * parent_->getSize().y) / game_background->GetLocalHeight());
   game_over_background->Move(indent_x, indent_y);
   game_over_background->SetScale(static_cast<float>(0.5 * parent_->getSize().x) / game_over_background->GetLocalWidth(),
-                            static_cast<float>(0.96 * parent_->getSize().y) / game_over_background->GetLocalHeight());
+                                 static_cast<float>(0.96 * parent_->getSize().y) / game_over_background->GetLocalHeight());
 
+}
+
+void Game1::CreateButtons() {
+  back_button = new Button(PATH_TO_RESOURCES"/buttons/back_button0.png",
+                           PATH_TO_RESOURCES"/buttons/back_button0.png",
+                           PATH_TO_RESOURCES"/buttons/back_button0.png",
+                           parent_,
+                           false);
+  back_button->SetMouseButtonReleasedEvent([this](const sf::Event& event) { ButtonLink(event); });
+  back_button->SetSize(parent_->getSize().x / 3, parent_->getSize().y / 6);
+  back_button->Move(static_cast<float>(game_background->GetWidth() * 1.18),
+                    static_cast<float>(game_background->GetHeight() * 0.84));
+  restart_button = new Button(PATH_TO_RESOURCES"/buttons/restart_button0.png",
+                              PATH_TO_RESOURCES"/buttons/restart_button0.png",
+                              PATH_TO_RESOURCES"/buttons/restart_button0.png",
+                              parent_,
+                              false);
+  restart_button->SetMouseButtonReleasedEvent([this](const sf::Event& event) { ReloadGame(event); });
+  restart_button->SetSize(parent_->getSize().x / 3, parent_->getSize().y / 6);
+  restart_button->Move(static_cast<float>(game_background->GetWidth() * 1.18),
+                       static_cast<float>(game_background->GetHeight() * 0.50));
+
+}
+
+void Game1::SetTileProperties() {
   tile_size = game_background->GetWidth() / width_of_net_in_tiles;
   height_of_net_in_tiles = game_background->GetHeight() / tile_size;
   indent_by_x = tile_size / 7;
+}
 
+void Game1::CreateDesignLines() {
   red_line.setSize(sf::Vector2f(game_background->GetWidth(),
                                 game_background->GetHeight() - tile_size * height_of_net_in_tiles));
   red_line.setPosition(indent_x, indent_y + tile_size * height_of_net_in_tiles);
   red_line.setFillColor(sf::Color(255, 0, 0));
+  white_line1.setSize(sf::Vector2f(parent_->getSize().x * 0.4, 5));
+  white_line2.setSize(sf::Vector2f(parent_->getSize().x * 0.4, 5));
+  white_line3.setSize(sf::Vector2f(parent_->getSize().x * 0.4, 5));
+  white_line4.setSize(sf::Vector2f(parent_->getSize().x * 0.4, 5));
+  white_line1.setFillColor(my_white);
+  white_line2.setFillColor(my_white);
+  white_line3.setFillColor(my_white);
+  white_line4.setFillColor(my_white);
+  white_line1.setPosition(game_background->GetWidth() * 2 * (0.5 + 0.02 + (1 - 0.5 - 0.02 - 0.4) / 2.),
+                          0.05 * game_background->GetHeight());
+  white_line2.setPosition(game_background->GetWidth() * 2 * (0.5 + 0.02 + (1 - 0.5 - 0.02 - 0.4) / 2.),
+                          0.22 * game_background->GetHeight());
+  white_line3.setPosition(game_background->GetWidth() * 2 * (0.5 + 0.02 + (1 - 0.5 - 0.02 - 0.4) / 2.),
+                          0.4 * game_background->GetHeight());
+  white_line4.setPosition(game_background->GetWidth() * 2 * (0.5 + 0.02 + (1 - 0.5 - 0.02 - 0.4) / 2.),
+                          0.8 * game_background->GetHeight());
+}
 
-  white_line.setSize(sf::Vector2f(parent_->getSize().x * 0.4, 5));
-  white_line.setFillColor(my_white);
-
+void Game1::CreateFigures() {
   figures_pos =
       {
           {{-tile_size, 0}, {-2 * tile_size, 0}, {0, 0}, {tile_size, 0}},
@@ -56,17 +106,16 @@ Game1::Game1() {
   figure = {figures[type_of_figure], figures[type_of_figure].size(), type_of_figure};
   type_of_figure = std::rand() % figures.size();
   next_figure = {figures[type_of_figure], figures[type_of_figure].size(), type_of_figure};
+}
 
-  color = GetRandomColor();
-  next_color = GetRandomColor();
-
+void Game1::SetFigureMovement() {
   speed_of_falling = 1;
   moves_by_x = 0;
   moves_by_y = 0;
   rotation = 0;
-  sf::Event event;
-  SetKeyPressedEvent( [this](const sf::Event& event){ CheckEvents(event); });
+}
 
+void Game1::CreateDataArrays() {
   is_tiled.resize(height_of_net_in_tiles + 2,
                   std::vector<bool>(width_of_net_in_tiles + 2, false));
   is_destroyed.resize(height_of_net_in_tiles + 2,
@@ -75,12 +124,9 @@ Game1::Game1() {
                                   std::vector<bool>(width_of_net_in_tiles + 2, false));
   mem_color.resize(height_of_net_in_tiles + 2,
                    std::vector<sf::Color>(width_of_net_in_tiles + 2, my_white));
+}
 
-  record = GetRecord();
-  coins = 0;
-
-  main_font.loadFromFile(PATH_TO_RESOURCES"/Game1/fonts/main_font.ttf");
-
+void Game1::CreateTextObjects() {
   SetText(record_text, main_font, "Record", 50, my_green,
           {static_cast<float>(game_background->GetWidth() * 1.40),
            static_cast<float>(game_background->GetHeight() * 0.13)});
@@ -90,22 +136,49 @@ Game1::Game1() {
   SetText(score_text, main_font, "Score", 50, my_blue,
           {static_cast<float>(game_background->GetWidth() * 1.20),
            static_cast<float>(game_background->GetHeight() * 0.31)});
+  SetText(score_value, main_font, std::to_string(score), 50, my_white,
+          {static_cast<float>(game_background->GetWidth() * 1.20),
+           static_cast<float>(game_background->GetHeight() * 0.24)});
   SetText(coins_text, main_font, "Coins", 50, my_orange,
           {static_cast<float>(game_background->GetWidth() * 1.65),
            static_cast<float>(game_background->GetHeight() * 0.31)});
+  SetText(coins_value, main_font, std::to_string(coins), 50, my_white,
+          {static_cast<float>(game_background->GetWidth() * 1.65),
+           static_cast<float>(game_background->GetHeight() * 0.24)});
   SetText(new_text, main_font, "New", 50, my_orange,
           {static_cast<float>(game_background->GetWidth() * 1.45),
            static_cast<float>(game_background->GetHeight() * 0.71)});
   SetText(back_text, main_font, "Back", 100, my_orange,
           {static_cast<float>(game_background->GetWidth() * 1.35),
-           static_cast<float>(game_background->GetHeight() * 0.85)});
+           static_cast<float>(game_background->GetHeight() * 0.84)});
   SetText(restart_text, main_font, "Restart", 100, my_blue,
           {static_cast<float>(game_background->GetWidth() * 1.27),
            static_cast<float>(game_background->GetHeight() * 0.50)});
   SetText(game_over_text, main_font, "I'm still hungry:(", 50, my_orange,
           {static_cast<float>(game_background->GetWidth() * 0.28),
            static_cast<float>(game_background->GetHeight() * 0.20)});
+}
 
+Game1::Game1() {
+  std::srand(time(NULL));
+  SetUpdateEvent([this] (double dt) {Update(dt); });
+  SetKeyPressedEvent( [this](const sf::Event& event){ CheckEvents(event); });
+  indent_x = static_cast<float>(0.02 * parent_->getSize().x);
+  indent_y = static_cast<float>(0.02 * parent_->getSize().y);
+  CreateBackground();
+  CreateButtons();
+  main_font.loadFromFile(PATH_TO_RESOURCES"/Game1/fonts/main_font.ttf");
+  //background music starts playing
+  SetTileProperties();
+  CreateDesignLines();
+  CreateFigures();
+  color = GetRandomColor();
+  next_color = GetRandomColor();
+  SetFigureMovement();
+  CreateDataArrays();
+  record = GetRecord();
+  coins = 0;
+  CreateTextObjects();
 }
 
 void Game1::SetText(sf::Text& target, const sf::Font& font, const std::string& text, int character_size,
@@ -135,6 +208,7 @@ int Game1::GetRecord() {
 
 void Game1::SetRecord(int my_record, int my_score) {
   int new_record = std::max(my_record, my_score);
+  record = new_record;
   std::ofstream record_file;
   record_file.open(PATH_TO_RESOURCES"/Game1/data_files/record.txt");
   record_file << std::to_string(new_record);
@@ -170,7 +244,7 @@ void Game1::CheckEvents(const sf::Event& event) {
 
 void Game1::DestroyRow(int i, int j) {
   if (transparency == start_transparency) {
-    //row_destroying_sound.play();
+    //row_destroying_sound starts playing
   }
   transparency -= speed_of_destruction * speed_of_falling;
   if (transparency < 2) {
@@ -192,34 +266,19 @@ void Game1::ReloadNet() {
   }
   type_of_figure = std::rand() % figures.size();
   figure = {figures[type_of_figure], figures[type_of_figure].size(), type_of_figure};
-  score = 0;
   speed_of_falling = 1;
 }
 
-void Game1::Draw() {
-  background->Draw();
-  game_background->Draw();
+void Game1::DrawNet() {
   for (int i = 0; i < height_of_net_in_tiles; ++i) {
     for (int j = 0; j < width_of_net_in_tiles; ++j) {
       tile.setPosition(indent_x + j * tile_size, indent_y + i * tile_size);
       parent_->draw(tile);
     }
   }
-  parent_->draw(red_line);
+}
 
-  white_line.setPosition(game_background->GetWidth() * 2 * (0.5 + 0.02 + (1 - 0.5 - 0.02 - 0.4) / 2.),
-                         0.05 * game_background->GetHeight());
-  parent_->draw(white_line);
-  white_line.setPosition(game_background->GetWidth() * 2 * (0.5 + 0.02 + (1 - 0.5 - 0.02 - 0.4) / 2.),
-                         0.22 * game_background->GetHeight());
-  parent_->draw(white_line);
-  white_line.setPosition(game_background->GetWidth() * 2 * (0.5 + 0.02 + (1 - 0.5 - 0.02 - 0.4) / 2.),
-                         0.4 * game_background->GetHeight());
-  parent_->draw(white_line);
-  white_line.setPosition(game_background->GetWidth() * 2 * (0.5 + 0.02 + (1 - 0.5 - 0.02 - 0.4) / 2.),
-                         0.8 * game_background->GetHeight());
-  parent_->draw(white_line);
-
+void Game1::DrawFigure() {
   for (size_t i = 0; i < figure.size; ++i) {
     sf::RectangleShape figure_tile(sf::Vector2f(tile_size - 2, tile_size - 2));
     figure_tile.setPosition(indent_x + 1 + figure.tiles[i].getPosition().x,
@@ -227,7 +286,9 @@ void Game1::Draw() {
     figure_tile.setFillColor(color);
     parent_->draw(figure_tile);
   }
+}
 
+void Game1::DrawNextFigure() {
   if (!is_active_game_over) {
     for (size_t i = 0; i < next_figure.size; ++i) {
       sf::RectangleShape figure_tile(sf::Vector2f(tile_size - 2, tile_size - 2));
@@ -239,8 +300,9 @@ void Game1::Draw() {
       parent_->draw(figure_tile);
     }
   }
+}
 
-  //maintain bottom correct
+void Game1::MaintainCorrectBottom() {
   for (int i = 0; i < height_of_net_in_tiles; ++i) {
     for (int j = 0; j < width_of_net_in_tiles; ++j) {
       if (is_destroyed[i][j]) {
@@ -251,36 +313,34 @@ void Game1::Draw() {
       if (supposed_to_be_destroyed[i][j]) {
         DestroyRow(i, j);
       }
-      if (is_tiled[i][j] && !in_process_of_destruction) {
+      if (is_tiled[i][j]) {
         sf::RectangleShape figure_tile(sf::Vector2f(tile_size - 2, tile_size - 2));
-        figure_tile.setPosition(indent_x + 1 + j * tile_size, indent_y + 1 + i * tile_size);
         figure_tile.setFillColor(mem_color[i][j]);
-        parent_->draw(figure_tile);
-      }
-      if (is_tiled[i][j] and in_process_of_destruction) {
-        if (i <= row_of_destruction) {
-          sf::RectangleShape figure_tile(sf::Vector2f(tile_size - 2, tile_size - 2));
-          figure_tile.setPosition(indent_x + 1 + j * tile_size, indent_y + 1 + (i - 1) * tile_size);
-          figure_tile.setFillColor(mem_color[i][j]);
-          parent_->draw(figure_tile);
-        }
-        if (i > row_of_destruction) {
-          sf::RectangleShape figure_tile(sf::Vector2f(tile_size - 2, tile_size - 2));
+        if (!in_process_of_destruction || (is_tiled[i][j] and in_process_of_destruction and i > row_of_destruction)) {
           figure_tile.setPosition(indent_x + 1 + j * tile_size, indent_y + 1 + i * tile_size);
-          figure_tile.setFillColor(mem_color[i][j]);
-          parent_->draw(figure_tile);
         }
+        if (in_process_of_destruction and i <= row_of_destruction) {
+          figure_tile.setPosition(indent_x + 1 + j * tile_size, indent_y + 1 + (i - 1) * tile_size);
+        }
+        parent_->draw(figure_tile);
       }
     }
   }
+}
 
-  SetText(score_value, main_font, std::to_string(score), 50, my_white,
-          {static_cast<float>(game_background->GetWidth() * 1.20),
-           static_cast<float>(game_background->GetHeight() * 0.24)});
-  SetText(coins_value, main_font, std::to_string(coins), 50, my_white,
-          {static_cast<float>(game_background->GetWidth() * 1.65),
-           static_cast<float>(game_background->GetHeight() * 0.24)});
-
+void Game1::Draw() {
+  background->Draw();
+  game_background->Draw();
+  parent_->draw(red_line);
+  parent_->draw(white_line1);
+  parent_->draw(white_line2);
+  parent_->draw(white_line3);
+  parent_->draw(white_line4);
+  DrawNet();
+  DrawFigure();
+  DrawNextFigure();
+  MaintainCorrectBottom();
+  back_button->Draw();
   parent_->draw(record_text);
   parent_->draw(record_value);
   parent_->draw(score_text);
@@ -288,29 +348,24 @@ void Game1::Draw() {
   parent_->draw(coins_text);
   parent_->draw(coins_value);
   parent_->draw(back_text);
-  if (!is_active_game_over) {
-    parent_->draw(new_text);
-  }
-
   if (is_active_game_over) {
     game_over_background->Draw();
+    restart_button->Draw();
     parent_->draw(restart_text);
     parent_->draw(game_over_text);
+  } else {
+    parent_->draw(new_text);
   }
 }
 
-void Game1::Update(double dt) {
-  tile_size = game_background->GetWidth() / width_of_net_in_tiles;
-  height_of_net_in_tiles = game_background->GetHeight() / tile_size;
-
+void Game1::SetNetTile() {
   tile.setSize(sf::Vector2f(tile_size, tile_size));
   tile.setFillColor(my_transparent);
   tile.setOutlineThickness(1);
   tile.setOutlineColor(my_skyblue);
+}
 
-  Figure prev_figure = figure;
-
-  // move by x
+void Game1::MoveByX(const Figure& prev_figure) {
   for (size_t i = 0; i < figure.size; ++i) {
     sf::Vector2f current_position = figure.tiles[i].getPosition();
     figure.tiles[i].setPosition(current_position.x + moves_by_x * tile_size, current_position.y);
@@ -324,16 +379,17 @@ void Game1::Update(double dt) {
       break;
     }
   }
+}
 
-  // move by y
+void Game1::MoveByY(const Figure &prev_figure) {
   for (size_t i = 0; i < figure.size; ++i) {
     if (is_tiled[static_cast<int>(figure.tiles[i].getPosition().y) /
         tile_size + 1][static_cast<int>(figure.tiles[i].getPosition().x) / tile_size] or
-        figure.tiles[i].getPosition().y > game_background->GetHeight() - tile_size) {
-      //gl.figure_placing_sound.play()
+        figure.tiles[i].getPosition().y > game_background->GetHeight() - tile_size - red_line.getSize().y) {
+      //figure_placing_sound starts playing
       if (static_cast<int>(figure.tiles[i].getPosition().y) / tile_size + 1 == is_tiled.size()) {
         is_tiled[static_cast<int>(figure.tiles[i].getPosition().y) /
-        tile_size][static_cast<int>(figure.tiles[i].getPosition().x) / tile_size] = true;
+            tile_size][static_cast<int>(figure.tiles[i].getPosition().x) / tile_size] = true;
       }
       for (int j = 0; j < figure.size; ++j) {
         is_tiled[static_cast<int>(figure.tiles[j].getPosition().y) /
@@ -354,8 +410,9 @@ void Game1::Update(double dt) {
     figure.tiles[i].setPosition(current_position.x,
                                 current_position.y + moves_by_y * tile_size + speed_of_falling);
   }
+}
 
-  // rotation
+void Game1::Rotate(const Figure &prev_figure) {
   sf::RectangleShape center_of_rotation = figure.tiles[0];
   if (rotation == 1 && figure.type != 1) {
     for (size_t i = 0; i < figure.size; ++i) {
@@ -374,15 +431,22 @@ void Game1::Update(double dt) {
       }
     }
   }
+}
 
-  // return to base
+void Game1::UpdateCounters() {
+  record_value.setString(std::to_string(record));
+  score_value.setString(std::to_string(score));
+  coins_value.setString(std::to_string(coins));
+}
+
+void Game1::ReturnToBase() {
   moves_by_x = 0;
   moves_by_y = 0;
   rotation = 0;
+}
 
-  // find destroyed rows
-  int current_row = height_of_net_in_tiles - 1;
-  int destroyed_rows = 0;
+void Game1::FindDestroyedRows() {
+  int current_row = height_of_net_in_tiles - 1, destroyed_rows = 0;
   for (int i = 0; i < height_of_net_in_tiles - 1; ++i) {
     int row = height_of_net_in_tiles - 1 - i;
     int number_of_tiled_in_row = 0;
@@ -410,29 +474,35 @@ void Game1::Update(double dt) {
     }
   }
   score += destroyed_rows * width_of_net_in_tiles * one_tile_cost;
-  coins += destroyed_rows * width_of_net_in_tiles * one_tile_cost / 100;
+  coins += destroyed_rows;
+}
 
-  //game over
+void Game1::GetGameOver() {
   for (int col = 0; col < width_of_net_in_tiles; ++col) {
     if (is_tiled[1][col]) {
       is_active_game_over = true;
-      //game_over_sound.play()
+      //game_over_sound starts playing
     }
   }
   if (is_active_game_over) {
     SetRecord(record, score);
     ReloadNet();
-    //pygame.mixer.music.pause()
+    // all music is gone
   }
-
 }
 
-void Game1::cont() {
-  SetEnableMod(false);
-}
-
-void Game1::stop(){
-  SetEnableMod(true);
+void Game1::Update(double dt) {
+  tile_size = game_background->GetWidth() / width_of_net_in_tiles;
+  height_of_net_in_tiles = game_background->GetHeight() / tile_size;
+  SetNetTile();
+  Figure prev_figure = figure;
+  MoveByX(prev_figure);
+  MoveByY(prev_figure);
+  Rotate(prev_figure);
+  ReturnToBase();
+  FindDestroyedRows();
+  UpdateCounters();
+  GetGameOver();
 }
 
 Game1* Game1::game_1_pointer = nullptr;
@@ -447,4 +517,7 @@ Game1* Game1::GetGame1() {
 Game1::~Game1() {
   background->Delete();
   game_background->Delete();
+  back_button->Delete();
+  restart_button->Delete();
+  game_1_pointer = nullptr;
 }
--- Global_Classes.h
+++ Global_Classes.h
@@ -384,19 +384,19 @@ struct Figure {
 class Game1 : Essence {
  private:
   Game1();
-  void CheckEvents(const sf::Event& event);
-  void DestroyRow(int i, int j);
-  sf::Color GetRandomColor();
-  int GetRecord();
-  void SetRecord(int my_record, int my_score);
-  void SetText(sf::Text& target, const sf::Font& font, const std::string& text, int character_size, sf::Color my_color,
-               sf::Vector2f pos);
-  void ReloadNet();
+
+  static Game1 *game_1_pointer;
+
+  Fon *background;
+
   Object* game_background;
   Object* game_over_background;
-  static Game1 *game_1_pointer;
-  Fon *background;
-  sf::Texture game_background_image, game_over_background_image;
+  sf::Texture game_background_image;
+  sf::Texture game_over_background_image;
+
+  Button* back_button;
+  Button* restart_button;
+
   static const int width_of_net_in_tiles = 15;
   int height_of_net_in_tiles;
   float indent_x;
@@ -405,7 +405,10 @@ class Game1 : Essence {
   sf::RectangleShape tile;
 
   sf::RectangleShape red_line;
-  sf::RectangleShape white_line;
+  sf::RectangleShape white_line1;
+  sf::RectangleShape white_line2;
+  sf::RectangleShape white_line3;
+  sf::RectangleShape white_line4;
 
   std::vector<std::vector<Coords>> figures_pos;
   std::vector<std::vector<sf::RectangleShape>> figures;
@@ -426,21 +429,31 @@ class Game1 : Essence {
 
   int start_transparency = 255;
   int transparency = start_transparency;
+
   int speed_of_destruction = 5;
   bool in_process_of_destruction = false;
   int row_of_destruction = -1;
   int counter_of_destructed_rows = 0;
   int max_value_before_speed_up = 5;
 
+  bool is_active_game_over = false;
   int score = 0;
-  int rows = 0;
+
   int one_tile_cost = 10;
   int coins;
   int record;
 
   sf::Font main_font;
-  sf::Text record_text, record_value, score_text, score_value, coins_text, coins_value, new_text, back_text,
-  restart_text, game_over_text;
+  sf::Text record_text;
+  sf::Text record_value;
+  sf::Text score_text;
+  sf::Text score_value;
+  sf::Text coins_text;
+  sf::Text coins_value;
+  sf::Text new_text;
+  sf::Text back_text;
+  sf::Text restart_text;
+  sf::Text game_over_text;
 
   sf::Color color;
   sf::Color next_color;
@@ -451,15 +464,47 @@ class Game1 : Essence {
   sf::Color my_white = {255, 255, 255};
   sf::Color my_transparent = {255, 255, 255, 0};
 
-  bool is_active_game_over = false;
-
  public:
   static Game1* GetGame1();
   Game1(const Game1&) = delete;
   Game1& operator=(const Game1&) = delete;
-  void stop();
-  void cont();
+
   void Update(double dt);
   void Draw();
+
+  void CreateBackground();
+  void CreateButtons();
+  void SetTileProperties();
+  void CreateDesignLines();
+  void CreateFigures();
+  void SetFigureMovement();
+  void CreateDataArrays();
+  void CreateTextObjects();
+  void DrawNet();
+  void DrawFigure();
+  void DrawNextFigure();
+  void MaintainCorrectBottom();
+  void SetNetTile();
+  void MoveByX(const Figure& prev_figure);
+  void MoveByY(const Figure& prev_figure);
+  void Rotate(const Figure& prev_figure);
+  void UpdateCounters();
+  void ReturnToBase();
+  void FindDestroyedRows();
+  void GetGameOver();
+
+  sf::Color GetRandomColor();
+  int GetRecord();
+  void SetRecord(int my_record, int my_score);
+  void SetText(sf::Text& target, const sf::Font& font, const std::string& text, int character_size, sf::Color my_color,
+               sf::Vector2f pos);
+
+  void CheckEvents(const sf::Event& event);
+  void DestroyRow(int i, int j);
+  void ReloadNet();
+
+  void ButtonLink(const sf::Event& event);
+  void ReloadGame(const sf::Event& event);
+
   ~Game1();
 };
